a16339fb7287986ec8c1810eee74d967528be3df
Joey Valones <joey.valones@unifiedmicro.systems>
2020-04-03 16:21:45 +0800

    fixed loading of appsettings.Development.json

--- AdefHelpDesk/Startup.cs
+++ AdefHelpDesk/Startup.cs
@@ -54,7 +54,7 @@ namespace AdefHelpDeskBase
             var builder = new ConfigurationBuilder()
            .SetBasePath(env.ContentRootPath)
            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-           .AddJsonFile($"appsettings{env.EnvironmentName}.json", optional: true)
+           .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
            .AddEnvironmentVariables();
 
             Configuration = builder.Build();
